b79eec7b11de39e1fbd27c8fca6f8f7cd7882c04
CelineZa <celinezanier@gmail.com>
2017-06-15 16:10:09 +0200

    Liste users

--- scripts/app.js
+++ scripts/app.js
@@ -13,4 +13,40 @@ $(function(){
 		index++; // On incrémente l'index.
 	},3000);
 
+
+	// Récupérer le nom des utilisateurs dans un fichier JSON et les ajouter dans une liste. 
+	var request = $.ajax({
+	  url: "https://jsonplaceholder.typicode.com/users", // Obligatoire
+	  method: "GET", // Obligatoire
+	  dataType: "json" // Optionnel
+	});
+	
+	request.done(function(data) { // data = toutes les données renvoyées par la requête (sous la forme d'un ARRAY)
+			var content = "";
+		data.forEach(function(element){
+			//console.log(element);
+			content += '<li><a href="">'+element.name+'</a></li>';
+		});
+
+		$("#categorie").html(content);
+	});
+	
+	//Autre manière de faire :
+	/*request.done(function(msg) {
+		console.log(msg);
+		console.log(msg[0].name);
+		console.log(msg.length);
+
+		$( "#categorie" ).html(""); // On vide le contenu du <ul> (#categorie), dans lequel on veut ajouter les noms.
+		
+		for(i = 0; i < msg.length; i++)
+		{
+			$( "#categorie" ).append('<li><a href="">'+msg[i].name+'</a></li>');
+		}
+	});*/
+	 
+	request.fail(function( jqXHR, textStatus ) {
+	  alert( "Request failed: " + textStatus );
+	});
+
 });
