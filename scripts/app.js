bbc1e0f37e40cd450d3545c03bee0807c78b222f
CelineZa <celinezanier@gmail.com>
2017-06-16 12:39:29 +0200

    Read more

--- scripts/app.js
+++ scripts/app.js
@@ -14,7 +14,7 @@ $(function(){
 	},3000);
 
 
-	// Récupérer le nom des utilisateurs dans un fichier JSON et les ajouter dans une liste. 
+	// Exercice : Récupérer le nom des utilisateurs dans un fichier JSON et les ajouter dans une liste. 
 	var request = $.ajax({
 	  url: "https://jsonplaceholder.typicode.com/users", // Obligatoire
 	  method: "GET", // Obligatoire
@@ -32,7 +32,7 @@ $(function(){
 
 		$("#categorie").html(content);
 
-		// Afficher l'id et l'email dans un console.info() lorsque l'on clique sur un noms
+		// Exercice : Afficher l'id et l'email dans un console.info() lorsque l'on clique sur un noms
 		$("#right_column ul > li").click(function(e){
 			e.preventDefault();
 			//console.log(data);
@@ -44,7 +44,7 @@ $(function(){
 			var ficheUser = $.ajax({
 			  url: "https://jsonplaceholder.typicode.com/users",
 			  method: "GET",
-			  data: { id : id_utilisateur[1] }, //On récupère l'id qui est à l'indice [1] du tableau id_utilisateur.
+			  data: { id : id_utilisateur[1] }, // On récupère les informations de l'utilisateur qui a l'id qui est égale à id_utilisateur[1].
 			  dataType: "json"
 			});
 
@@ -69,7 +69,82 @@ $(function(){
 		}
 	});*/
 	 
-	request.fail(function( jqXHR, textStatus ) {
+	request.fail(function(jqXHR, textStatus) {
 	  alert( "Request failed: " + textStatus );
 	});
+
+
+	// Exercice : Afficher les 4 premiers posts (en dessous du carrousel). Si la description est supérieure à 100 caractères, on met “...”.
+	$.ajax({
+	  url: "https://jsonplaceholder.typicode.com/posts", 
+	  method: "GET",
+	  dataType: "json"
+	})	
+
+	.done(function(dataPosts) {
+		console.log(dataPosts);
+
+		for(let i = 0; i < 4; i++)
+		{
+			$('.one_quarter > strong').eq(i).text(dataPosts[i].title.slice(0, 15)); // Texte prend le texte brut. Il n'interprète pas les balises HTML contrairement à .html()
+			
+			$('.affichePost').eq(i).text(dataPosts[i].body.slice(0, 100) + "...");
+
+			$('.one_quarter a').eq(i).click(function(e){
+				e.preventDefault();
+
+				if($(this).text() != 'Read less >') // Si le texte dans la balise <a> est égal à "Read less >" on affiche le texte en entier.
+				{
+					$('.affichePost').eq(i).text(dataPosts[i].body);
+					$(this).text('Read less >');
+				}		
+				else  // Sinon on affiche le texte raccourci.
+				{
+					$('.affichePost').eq(i).text(dataPosts[i].body.slice(0, 100) + "...");
+					$(this).text('Read more >');
+				}
+			});
+		}	
+	})
+
+	.fail(function(jqXHR, textStatus) {
+	  alert( "Request failed: " + textStatus );
+	});
+
+/*	Ma version :
+
+	var affichePost = $.ajax({
+	  url: "https://jsonplaceholder.typicode.com/posts", 
+	  method: "GET",
+	  dataType: "json"
+	});
+	
+	affichePost.done(function(dataPost) {
+		//console.log(dataPost);
+
+		for(let i = 0; i < 4; i++)
+		{
+			let titre = dataPost[i].title.slice(0, 15); // On coupe le titre au bout de 15 caractères.
+			$('.one_quarter > strong').eq(i).html(titre); // Au 1er tour, on affiche le 1er titre dans le 1er strong, au 2ème tour, le 2ème titre dans le 2ème strong, ...
+
+			//console.log(dataPost[i].body.slice(0, 100));
+			let description = dataPost[i].body.slice(0, 100); // On coupe la description au bout de 100 caractères.
+			$('.affichePost').eq(i).html("<p>"+description+"...</p>");
+
+			//***** NE FONCTIONNE PAS ! ******
+			$('.more').click(function(e){
+				e.preventDefault();
+
+				$('.affichePost').html(dataPost[i].body);
+				$(this).html('Read less');
+			});	
+			//********************************
+		}
+
+	});
+	 
+	affichePost.fail(function( jqXHR, textStatus ) {
+	  alert( "Request failed: " + textStatus );
+	});
+*/
 });
